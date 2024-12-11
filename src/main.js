a1e57e2383da6866b9e5e90add083cd1786e49d8
vo-n <guilhermehigaf@gmail.com>
2024-12-11 16:39:34 -0300

    Exercício de TypeScript

---
+++ src/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Função para multiplicar dois números 
+const multiplicar = (num1, num2) => num1 * num2;
+console.log(multiplicar(2, 5));
+// Função para fazer uma saudação
+const dizOi = (name) => `${name} diz "Oi".`;
+console.log(dizOi("Guilherme"));
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+// Função para multiplicar dois números 
+
+const multiplicar = (num1: number, num2: number) => num1 * num2;
+
+console.log(multiplicar(2, 5));
+
+// Função para fazer uma saudação
+
+const dizOi = (name: string): string => `${name} diz "Oi".`
+
+console.log(dizOi("Guilherme"));
+
